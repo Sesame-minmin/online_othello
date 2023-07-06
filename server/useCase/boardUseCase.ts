99323cc69b84043e4a73ab82c37ee3fe061c429a
Sesame-minmin <s1b102200969@toyo.jp>
2023-07-06 17:58:19 +0900

    八方向はできたけど()

--- server/useCase/boardUseCase.ts
+++ server/useCase/boardUseCase.ts
@@ -25,24 +25,40 @@ const directions: number[][] = [
 //y=>ユーザーがクリックしたｙ座標
 //by=>横への移動方向、ｘにくっつく
 //ve=>縦への移動方向、ｙにくっつく
-const next = (x: number, y: number, by: number, ve: number, userId: UserId) => {
+//if (board[y + ve][x + by] === 3 - colorUseCase.createColor(userId)) {
+//count += 1;
+//next(x + by, y + ve, by, ve, userId, count);
+//} else if (board[y + ve][x + by] === colorUseCase.createColor(userId)) {
+//return [y, x, count];
+//自分の石と対になる
+//}
+
+//while (board[y + count * ve][x + count * by] === 3 - colorUseCase.createColor(userId)) {
+// count += 1;
+//if (board[y + count * ve][x + count * by] === colorUseCase.createColor(userId)) {
+//return [y + count * ve, x + count * by, count];
+//}
+//}
+const next = (x: number, y: number, by: number, ve: number, userId: UserId, count: number) => {
   if (0 < x + by && x + by < 8 && 0 < y + ve && y + ve < 8) {
     if (board[y + ve][x + by] === 3 - colorUseCase.createColor(userId)) {
-      next(x + by, y + ve, by, ve, userId);
+      count += 1;
+      next(x + by, y + ve, by, ve, userId, count);
     } else if (board[y + ve][x + by] === colorUseCase.createColor(userId)) {
-      return [y, x];
+      return [y, x, count];
       //自分の石と対になる
     }
   }
-  return [10];
+  return [111];
 };
 
-const turnColor = (x: number, y: number, di: number[], goal: number[], userId: UserId) => {
-  if (goal[0] !== 10) {
-    for (let i = goal[1]; i !== x; i -= di[1]) {
-      for (let j = goal[0]; j !== y; j -= di[0]) {
-        board[j][i] = colorUseCase.createColor(userId);
-      }
+const turnColor = (di: number[], goal: number[], userId: UserId) => {
+  if (goal[0] !== 111) {
+    while (goal[2] >= 0) {
+      board[goal[0]][goal[1]] = colorUseCase.createColor(userId);
+      goal[0] -= di[0];
+      goal[1] -= di[1];
+      goal[2] -= 1;
     }
   }
 };
@@ -54,8 +70,7 @@ export const boardUseCase = {
     for (const di of directions) {
       if (0 < x + di[1] && x + di[1] < 8 && 0 < y + di[0] && y + di[0] < 8) {
         if (board[y + di[0]][x + di[1]] === 3 - colorUseCase.createColor(userId)) {
-          turnColor(x, y, di, next(x + di[1], y + di[0], di[1], di[0], userId), userId);
-          board[y][x] = colorUseCase.createColor(userId);
+          turnColor(di, next(x + di[1], y + di[0], di[1], di[0], userId, 1), userId);
         }
       }
     }
